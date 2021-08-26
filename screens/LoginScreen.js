621ac645134089d63f9d635ba1a2f4fb7acb5f57
stelcreativ <45742221+stelcreativ@users.noreply.github.com>
2021-08-26 17:47:25 +0100

    base commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, StyleSheet, TextInput } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 export default function LoginScreen() {
     return (
